b54fa62cbca33d53576300dca65a7706e0af1f14
Rene Cocom <fasnimo@gmail.com>
2019-08-06 01:41:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,20 @@
 # require libraries/modules here
-require 'Nokogiri'
-require 'pry'
+# require 'Nokogiri'
+# require 'pry'
+# def create_project_hash
+#   html = File.read('fixtures/kickstarter.html')
+#
+#   kickstarter = Nokogiri::HTML(html)
+#   binding.pry
+# end
+#
+# create_project_hash
+
+require "nokogiri"
+require "pry"
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
